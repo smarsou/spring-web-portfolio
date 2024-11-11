e434a6e38593e092f576547377e75d896b856728
Zintoulou <34sousou34@gmail.com>
2024-11-11 09:06:14 +0100

    Update tests

--- src/test/java/com/soulaiman/portfolio/AuthApplicationTests.java
+++ src/test/java/com/soulaiman/portfolio/AuthApplicationTests.java
@@ -2,10 +2,14 @@ package com.soulaiman.portfolio;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -18,26 +22,27 @@ import static org.springframework.security.test.web.servlet.setup.SecurityMockMv
 import static org.hamcrest.Matchers.containsString;
 
 @SpringBootTest
-@AutoConfigureMockMvc
 @ContextConfiguration(classes = {PortfolioApplication.class, WebSecurityConfig.class})
+@WebAppConfiguration
 public class AuthApplicationTests {
     
     @Autowired
-    private MockMvc mockMvc;
-
-    @Autowired
     private WebApplicationContext context;
 
+    private MockMvc mockMvc;
+
     @BeforeEach
-	void setup() throws Exception{
-		this.mockMvc = MockMvcBuilders
-				.webAppContextSetup(context)
-				.apply(springSecurity()) 
-				.build();
-	}
+    void setup() throws Exception{
+        this.mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity()) 
+                .build();
+    }
 
     @Test
+    @WithMockUser("ADMIN")
     void adminPageShouldBeOk() throws Exception{
         this.mockMvc.perform(get("/admin")).andDo(print()).andExpect(status().isOk()).andExpect(content().string(containsString("action=\"/admin/saveProject\"")));
     }
+
 }
